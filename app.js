523c7153578aab365bf5d8d646371c21916f8924
Youness Dendane <youness.dendane1@uqac.ca>
2019-05-13 10:15:45 -0400

    Added search functionnality and redirected to the proper page

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-document.getElementById('search').addEventListener('submit', function(e) {
+document.getElementById('search').addEventListener('submit', function (e) {
   e.preventDefault();
+
   const searchText = document.querySelector('#search-text').value;
   console.log(searchText);
+
+  // Search for a recipe and redirect 
+  // to the search_results.html page
+  getSearchResults(searchText);
 });
+
+function getSearchResults(search) {
+  sessionStorage.setItem('search', search);
+  window.location = 'search_results.html';
+}
