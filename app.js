2a9c719727566631b3247985e606d7e9e9347f11
Youness Dendane <youness.dendane1@uqac.ca>
2019-05-13 14:16:05 -0400

    Added click functionnality on a recipe to have further details

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
+const results = [
+  {
+    'id': '00001',
+    'name': 'Old School Tajine',
+    'difficulty': '2',
+    'total_time': '45',
+    'poster': 'https://burst.shopifycdn.com/photos/moroccan-meal-in-tagine.jpg?width=4460&height=4460&exif=1&iptc=1'
+  },
+  {
+    'id': '00002',
+    'name': 'Tajine bl bar9o9',
+    'difficulty': '3',
+    'total_time': '50',
+    'poster': 'https://www.bladi.net/IMG/arton7256.jpg'
+  },
+  {
+    'id': '00003',
+    'name': 'Pizza Margarita',
+    'difficulty': '1',
+    'total_time': '30',
+    'poster': 'https://burst.shopifycdn.com/photos/margarita-pizza-and-fresh-tomatos.jpg?width=4460&height=4460&exif=1&iptc=1'
+  },
+];
+const recipes = document.querySelector('#recipes');
 const searchForm = document.querySelector('#search');
+
 if (searchForm) {
 
   document.getElementById('search').addEventListener('submit', function (e) {
@@ -19,30 +44,6 @@ function getSearchResults(search) {
 }
 
 function displayResults() {
-  const results = [
-    {
-      'id': '00001',
-      'name': 'Old School Tajine',
-      'difficulty': '2',
-      'total_time': '45',
-      'poster': 'https://burst.shopifycdn.com/photos/moroccan-meal-in-tagine.jpg?width=4460&height=4460&exif=1&iptc=1'
-    },
-    {
-      'id': '00002',
-      'name': 'Tajine bl bar9o9',
-      'difficulty': '3',
-      'total_time': '50',
-      'poster': 'https://www.bladi.net/IMG/arton7256.jpg'
-    },
-    {
-      'id': '00003',
-      'name': 'Pizza Margarita',
-      'difficulty': '1',
-      'total_time': '30',
-      'poster': 'https://burst.shopifycdn.com/photos/margarita-pizza-and-fresh-tomatos.jpg?width=4460&height=4460&exif=1&iptc=1'
-    },
-  ];
-  const resultsDOM = document.querySelector('#recipes');
   let output = '';
 
   results.forEach(element => {
@@ -60,15 +61,39 @@ function displayResults() {
     }
 
     output = `
-      <div class='col-md-3'>
+      <div class='col-md-3' id='${element.id}'>
         <div class='well text-center'>
           <img class='poster' src='${element.poster}' />
-          <h3>${element.name}</h3>
+          <a href='recipe_details.html'><h3>${element.name}</h3></a>
           ${difficulty}
         </div>
       </div>
     `;
 
-    resultsDOM.innerHTML += output;
+    recipes.innerHTML += output;
   });
+}
+
+// Event: On a recipe click
+if (recipes) {
+  recipes.addEventListener('click', (e) => {
+    const recipeId = e.target.parentNode.parentNode.parentNode.getAttribute('id');
+
+    // Adding the id to session storage
+    const recipe = JSON.stringify(getRecipeById(recipeId));
+    sessionStorage.setItem('recipe_details', recipe);
+  });
+}
+
+function getRecipeById(id) {
+
+  // Search for the element
+  for (let index = 0; index < results.length; index++) {
+    const element = results[index];
+
+    if (element.id == id)
+      return element;
+  }
+
+  return null;
 }
